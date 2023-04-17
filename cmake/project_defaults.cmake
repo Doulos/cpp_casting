edec21939013403f420e3582d1a70badc60a68c7
David C Black <david.black@doulos.com>
2023-04-17 16:36:54 -0500

    Update cmake
    Signed-off-by: David C Black <david.black@doulos.com>

--- cmake/project_defaults.cmake
+++ cmake/project_defaults.cmake
@@ -1,37 +1,8 @@
 #!cmake .
 cmake_minimum_required ( VERSION 3.21 )
 
-# Header style guard for multiple inclusion protection
-if( DEFINED PROJECT_DEFAULTS )
-  return()
-endif()
-set( MODERN_CMAKE_UTILS ON )
+include_guard( GLOBAL )
 
-#-------------------------------------------------------------------------------
-# Extend module path
-#-------------------------------------------------------------------------------
-if( DEFINED ENV{CMAKE_PREFIX_PATH} )
-  foreach( _dir $ENV{CMAKE_PREFIX_PATH} )
-    if( EXISTS "${_dir}" )
-      list( INSERT CMAKE_MODULE_PATH 1 "${_dir}" )
-    endif()
-  endforeach()
-endif()
-list( REMOVE_DUPLICATES CMAKE_MODULE_PATH )
-
-#-------------------------------------------------------------------------------
-# Increase sensitivity to all warnings
-#-------------------------------------------------------------------------------
-include( strict )  # << Report as many compilation issues as able
-
-#------------------------------------------------------------------------------
-# Choose minimum C++ standard for remainder of code
-#------------------------------------------------------------------------------
-set( CMAKE_CXX_STANDARD          17 CACHE STRING "C++ standard to build all targets." )
-set( CMAKE_CXX_STANDARD_REQUIRED 17 CACHE BOOL   "The CMAKE_CXX_STANDARD selected C++ standard is a requirement." )
-
-enable_testing()
-
-include( set_target )
+include( policy )
 
 # vim:syntax=cmake:nospell
--- cmake/strict.cmake
+++
@@ -1,7 +0,0 @@
-if (MSVC)
-    # warning level 4
-    add_compile_options( /W4 )
-else()
-    # lots of warnings
-    add_compile_options( -Wall -Wextra -pedantic )
-endif()
